597a3c5adb3fb907561dc1acc30c6572c27e4c34
NnatuanyaFrankOguguo <nnatuanyafrank@gmail.com>
2024-11-08 13:25:50 +0100

    ok

--- backend/dist/models/Users.js
+++ backend/dist/models/Users.js
@@ -1,1 +1,98 @@
-export {};
+import bcrpyt from "bcrypt";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import 'dotenv/config';
+const userSchema = new mongoose.Schema({
+    fname: {
+        type: 'string',
+        required: [true, 'First name is required'],
+        minlength: 2,
+        maxlength: 20
+    },
+    lname: {
+        type: 'string',
+        required: [true, 'Last name is required'],
+        minlength: 2,
+        maxlength: 20
+    },
+    email: {
+        type: 'string',
+        required: [true, 'Email is required'],
+        unique: true,
+        match: [/\S+@\S+\.\S+/, 'Please enter a valid email']
+    },
+    password: {
+        type: 'string',
+        required: [true, 'Password is required'],
+        minlength: 8,
+        select: false // Do not return password in response
+    },
+    phoneNumber: {
+        type: Number,
+        required: false,
+        minlength: 10,
+        maxlength: 15
+    },
+    address: [
+        {
+            address1: {
+                type: 'string',
+                required: false,
+            },
+            city: {
+                type: 'string',
+                required: false,
+            },
+            state: {
+                type: 'string',
+                required: false,
+            },
+            country: {
+                type: 'string',
+                required: false,
+            },
+            zipCode: {
+                type: 'Number',
+                required: false,
+            }
+        }
+    ],
+    role: {
+        type: String,
+        default: 'user'
+    },
+    avatar: {
+        public_id: {
+            type: String,
+            required: true,
+        },
+        url: {
+            type: String,
+            required: true,
+        }
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+}, { timestamps: true });
+// Hash password before saving to database
+userSchema.pre('save', async function (next) {
+    const user = this;
+    if (!user.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrpyt.genSalt(10);
+    user.password = await bcrpyt.hash(user.password, salt);
+    next();
+});
+/*writing the generate token function */
+userSchema.methods.getJwtToken = function () {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_SECRET_EXPIRATION });
+};
+// compare password 
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrpyt.compare(candidatePassword, this.password);
+};
+const User = mongoose.model('User', userSchema);
+export default User;
---
+++ backend/src/models/Users.ts
@@ -0,0 +1,137 @@
+
+import bcrpyt from "bcrypt";
+import jwt from "jsonwebtoken";
+import mongoose, {Document, Schema} from "mongoose";
+import 'dotenv/config'
+
+// Define an interface representing a User document in MongoDB
+interface IUser extends Document {
+    fname: string;
+    lname: string;
+    email: string;
+    password: string;
+    role: string;
+    phoneNumber?: number;
+    address?: {
+      address1?: string;
+      city?: string;
+      state?: string;
+      country?: string;
+      zipCode?: number;
+    };
+    avatar?: {
+        public_id: string;
+        url: string;
+    }
+    isVerified: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+    
+  }
+  
+
+const userSchema: Schema<IUser> = new mongoose.Schema({
+    fname: {
+        type: 'string',
+        required: [true, 'First name is required'],
+        minlength: 2,
+        maxlength: 20
+    },
+    lname: {
+        type: 'string',
+        required: [true, 'Last name is required'],
+        minlength: 2,
+        maxlength: 20
+    },
+    email: {
+        type: 'string',
+        required: [true, 'Email is required'],
+        unique: true,
+        match: [/\S+@\S+\.\S+/, 'Please enter a valid email']
+    },
+    password: {
+        type: 'string',
+        required: [true, 'Password is required'],
+        minlength: 8,
+        select: false  // Do not return password in response
+    },
+    phoneNumber: {
+        type: Number,
+        required: false,
+        minlength: 10,
+        maxlength: 15
+    },
+    address: [
+        {
+            address1: {
+                type: 'string',
+                required: false,
+            },
+            city: {
+                type: 'string',
+                required: false,
+            },
+            state: {
+                type: 'string',
+                required: false,  
+            },
+            country: {
+                type: 'string',
+                required: false,
+            },
+            zipCode: {
+                type: 'Number',
+                required: false,
+            }
+        }
+    ],
+    role : {
+        type: String,
+        default: 'user'
+    },
+    avatar: {
+        public_id: {
+            type: String,
+            required: true,
+        },
+        url: {
+            type: String,
+            required: true,
+        }
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    
+
+}, 
+{timestamps: true});
+
+// Hash password before saving to database
+
+userSchema.pre('save', async function(next) {
+    const user = this as IUser;
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    const salt = await bcrpyt.genSalt(10);
+    user.password = await bcrpyt.hash(user.password, salt);
+    next();
+});
+
+/*writing the generate token function */
+userSchema.methods.getJwtToken = function (): string {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET as string, { expiresIn: process.env.JWT_SECRET_EXPIRATION });
+}
+
+// compare password 
+
+userSchema.methods.comparePassword = async function(candidatePassword: string): Promise<boolean> {
+    return await bcrpyt.compare(candidatePassword, this.password);
+}
+
+const User = mongoose.model<IUser>('User', userSchema);
+
+export default User;
