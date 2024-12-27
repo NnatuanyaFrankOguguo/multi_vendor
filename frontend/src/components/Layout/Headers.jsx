431b43ecf41e843163277614e30a9db75e2b1431
NnatuanyaFrankOguguo <nnatuanyafrank@gmail.com>
2024-12-27 22:32:25 +0100

    done with the search filter logic for each products that when a user search it brings out the lists of goods with those letters and when the users clicks on it, it takes them to a route/page with the product name on the url as parameter for more details.

--- frontend/src/components/Layout/Headers.jsx
+++ frontend/src/components/Layout/Headers.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import styles from '../../styles/styles'
 import { Link } from 'react-router-dom'
 import leaf from '../../assets/leaf.png'
+import {productData} from '../../static/data'
+import { AiOutlineSearch } from 'react-icons/ai'
 
 const Header = () => {
 
@@ -13,7 +15,11 @@ const Header = () => {
         const term = e.target.value
         setSearchTerm(term);
 
-        const filteredProducts = productData
+        const filteredProducts = productData && productData.filter((product) => 
+            product.name.toLowerCase().includes(term.toLowerCase())
+
+        )
+        setSearchData(filteredProducts);
     }
 
   return (
@@ -27,8 +33,39 @@ const Header = () => {
             </div>
             {/* searchbox */}
             <div className='w-[50%] relative'>
-                <input type="text" name="" placeholder="Search Product..."  />
+                <input type="text" name="" placeholder="Search Product..." value={searchTerm} onChange={handleSearch} 
+                className='h-[40px] w-full px-2 border-green-300 border-[2px] rounded-md' />
+                <AiOutlineSearch size={30} className='absolute right-2 top-1.5 cursor-pointer' />
+                {
+                    searchData && searchData.length !== 0 ? (
+                        <div className="absolute min-h-[30vh] bg-slate-50 shadow-sm-2 z-[9] p-4">
+                            {
+                                searchData && searchData.map((product, index) => {
+                                    const d = product.name
+
+                                    const product_name = d.replace(/\s+/g, '-'); //to remove the spaces on the initial name so each word will be able to come up in the search
+                                    return (
+                                        <Link to={`/product/${product_name}`}>
+                                            <div className="w-full flex items-start-py-3">
+                                                <img src={product.image_Url[0].url} alt="" className='w-[40px] h-[40px] mr-[10px]' />
+                                                <h1>{product.name}</h1>
+                                            </div>
+                                        </Link>
+                                        // <Link to={`/product/${product_name}`} key={index} className='flex items-center space-x-2'>
+                                        //     <img src={product.image} alt={product.name} className='h-10 w-10' />
+                                        //     <p className='text-base text-blue-900 font-semibold'>{product.name}</p>
+                                        // </Link>
+                                    )
+                                })
+                            }
+                        </div>
+
+                    ) : null 
+                
+                }
             </div>
+
+           
 
         </div>
     </div>
